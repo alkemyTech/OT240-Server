38b2cff02a575b35e44c9b1e229ee2bdbb7ddf4c
Tomas Paez <paeztms@gmail.com>
2022-07-23 16:34:24 -0300

    fix role validation middleware to match toke validation implementation

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,7 +1,7 @@
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req?.user?.role) return res.sendStatus(401);
-    const isAuthorized = allowedRoles.includes(req.user.role);
+    if (!req?.role) return res.sendStatus(401);
+    const isAuthorized = allowedRoles.includes(req.role);
     if (!isAuthorized) return res.sendStatus(401);
     next();
   };
