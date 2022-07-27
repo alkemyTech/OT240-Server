6ebf80455e2783ac5932a703af5863a0633bef46
Tomas Paez <paeztms@gmail.com>
2022-07-26 23:16:19 -0300

    create seed implementation to fill/empty "testimonials" table

--- seeders/20220727011212-create-demo-testimonials.js
+++ seeders/20220727011212-create-demo-testimonials.js
@@ -1,24 +1,33 @@
 'use strict';
 
+const content =
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, sit laboriosam qui molestias sed nihil deleniti incidunt. Reprehenderit eum inventore alias quisquam velit? Reprehenderit fugit sint ut cupiditate maiores quae.';
+const names = [
+  'Juana Molina',
+  'Pedro Sanchez',
+  'Juliana Torres',
+  'Federico Robles',
+  'Maria Fernandez',
+  'Norberto Molinari',
+];
+const image = 'https://image-bucket.com/test-image.png';
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Testimonials',
+      names.map((name) => ({ name, content, image })),
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Testimonials', null, {});
+  },
 };
+
+// To fill the table with mock testimonials run:
+// npx sequelize-cli db:seed --seed 20220727011212-create-demo-testimonials.js
+
+// To empty the table "testimonials" run:
+// npx sequelize-cli db:seed:undo --seed 20220727011212-create-demo-testimonials.js
