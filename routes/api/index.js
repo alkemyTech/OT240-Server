f7b8faf7b5a36e43f7c39e54d4bdafc4b64981ae
Tomas Paez <paeztms@gmail.com>
2022-07-26 22:15:23 -0300

    fix routes that doesn't need auth or role (GET methods) on news entries

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,7 +6,7 @@ const verifyRoles = require('../../middleware/verifyRoles');
 const ROLES_LIST = require('../../config/rolesList');
 
 router.use('/auth', require('./auth.route'));
-router.use('/news', validateToken, verifyRoles(ROLES_LIST.Admin), require('./news.route'));
+router.use('/news', require('./news.route'));
 router.use('/members', require('./members.route'));
 router.use('/contacts', require('./contacts.route'));
 
--- routes/api/news.route.js
+++ routes/api/news.route.js
@@ -3,25 +3,28 @@ const router = express.Router();
 const { check } = require('express-validator');
 
 const newsController = require('../../controllers/news.controller');
+const validateToken = require('../../middleware/validateToken');
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/rolesList');
 
 //@route    GET /api/news/:id
 //@desc     Retrieve entry of type: "news" by id
-//@access   Private (Admin)
+//@access   Public
 router.get('/:id', newsController.getNewById);
 
 //@route    GET /api/news
 //@desc     Retrieve all entries of type: "news"
-//@access   Private (Admin)
+//@access   Public
 router.get('/', newsController.getNews);
 
 //@route    POST /api/news
 //@desc     Post entry of type: "news"
 //@access   Private (Admin)
-router.post('/', newsController.postNew);
+router.post('/', validateToken, verifyRoles(ROLES_LIST.Admin), newsController.postNew);
 
 //@route    PUT /api/news
 //@desc     Actualizar una novedad existente
-//@access   Private
+//@access   Private (Admin)
 router.put(
   '/:id',
   [check('name', 'name must be string').isString()],
@@ -29,12 +32,14 @@ router.put(
   [check('image', 'image must be string').isString()],
   [check('categoryId', 'categoryId must be integer').isInt()],
   [check('type', 'type must be string').isString()],
+  validateToken,
+  verifyRoles(ROLES_LIST.Admin),
   newsController.putNews
 );
 
 //@route    DELETE /api/news/:id
 //@desc     Post entry of type: "news"
 //@access   Private (Admin)
-router.delete('/:id', newsController.deleteNew);
+router.delete('/:id', validateToken, verifyRoles(ROLES_LIST.Admin), newsController.deleteNew);
 
 module.exports = router;
