72076dc74771934d022625a02eab62e59ab89049
Tomas Paez <paeztms@gmail.com>
2022-08-24 02:48:52 -0300

    cleanup to fix bugs

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API router
 app.use('/api', require('./routes/api/index'));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use('*', (req, res) => {
+	res.json('404 NOT FOUND');
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(PORT, () => console.log(`>> Listening on port ${PORT}`));
